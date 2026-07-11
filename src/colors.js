fd0c0452865917938d6e43d4d107a8c170d07122
Prins Go <prinsgoofficial@gmail.com>
2026-07-11 17:50:51 +0530

    Add files via upload

---
+++ src/colors.js
@@ -0,0 +1,20 @@
+export const colors = {
+  primary: '#FF7A1A',
+  primaryDark: '#E85D00',
+  primaryLight: '#FFA45C',
+  black: '#0B0B0F',
+  darkCard: '#16161C',
+  white: '#FFFFFF',
+  gray: '#8A8A94',
+  lightGray: '#F2F2F5',
+  border: '#E8E8ED',
+  success: '#22C55E',
+  danger: '#EF4444',
+  warning: '#F59E0B',
+  text: '#1A1A1F',
+  textMuted: '#6B6B75',
+};
+
+export const gradients = {
+  hero: ['#1C1C24', '#0B0B0F'],
+};
